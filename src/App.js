5f14d36912ada30c602b1b45f15fee143b3711ea
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2021-10-29 18:03:13 +0900

    comment out for hands-on

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { bunzz } from "bunzz-sdk";
+// [step1]: import SDK
+// import { bunzz } from "bunzz-sdk";
 
 import "./App.css";
 
-// configuration
+// [step2]: configuration
 const DAPP_ID = "YOUR_DAPP_ID";
 const API_KEY = "YOUR_API_KEY";
 
@@ -17,19 +18,26 @@ const init = async () => {
 
 
 
-// Application
+
+
 const App = () => {
   const [contract, setContract] = useState();
   const [value, setValue] = useState(0);
-  const [userAddress, setUserAddress] = useState("hoge");
+  const [userAddress, setUserAddress] = useState("");
 
   useEffect(() => {
     const setup = async () => {
       try {
         const handler = await init();
-        const userAddress = await handler.getSignerAddress();
+
+        // [step3]: Get your address from Metamask
+        // const userAddress = await handler.getSignerAddress();
+
+        // [step4]: Get a contract object
+        // const contract = await handler.getContract("ERC20");
+
         setUserAddress(userAddress);
-        setContract(handler.getContract("ERC20"));
+        setContract(contract);
       } catch (error) {
         console.error(error);
       }
@@ -41,12 +49,14 @@ const App = () => {
   const handleChange = (e) => setValue(e.target.value);
 
   const submit = async () => {
-    await contract.mint(userAddress, value);
+    //[step5]: Call a function of smart contract
+    // await contract.mint(userAddress, value);
     alert("Transaction was sent in success🎉");
   };
 
   return (
     <div className="App App-header">
+      <p>You can mint your ERC20 if you're the owner</p>
       <input value={value} onChange={handleChange} type="text" />
       <button onClick={submit}>mint</button>
     </div>
